212733109dbbd71752f47c265dfeb0f0a165a0ca
Nico <nlfernandez3@gmail.com>
2023-04-21 11:39:12 -0300

    Creación de las funciones para las operaciones matematicas de la calculadora

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    if (operator === "+") {
+        return add(a, b);
+    } else if (operator === "-") {
+        return subtract(a, b);
+    } else if (operator === "*") {
+        return multiply(a, b);
+    } else if (operator === "/") {
+        return divide(a, b);
+    }
+}
+
+let n1 = 0;
+let n2 = 0;
+let op = "";
